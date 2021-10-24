d6ea290900a0443623692f822fa61b93a8f58191
abzara20 <azaragozapalos@gmail.com>
2021-10-23 20:13:41 -0400

    make mobile nav function

--- app/app.js
+++ app/app.js
@@ -15,8 +15,33 @@ function route() {
 function initListeners() {
   $(window).on("hashchange", route);
   route();
+
+  $(".mobile").click(function (e) {
+    gsap.to(".mobile-nav", {
+      duration: 0.5,
+      x: 0,
+      opacity: 1,
+    });
+  });
+
+  $(".mobile-bg").click(function (e) {
+    hideMenu();
+  });
+
+  $(".mobile-menu a").click(function (e) {
+    hideMenu();
+  });
+}
+
+function hideMenu() {
+  gsap.to(".mobile-nav", {
+    duration: 0.5,
+    x: 300,
+    opacity: 0,
+  });
 }
 
 $("document").ready(function () {
   initListeners();
+  gsap.set($(".mobile-nav"), { opacity: 0, x: 300 });
 });
